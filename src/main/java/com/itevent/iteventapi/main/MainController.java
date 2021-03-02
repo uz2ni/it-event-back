731dafe023d402c0ec950e969d203b2acaa71b64
uz2ni <uzini_@likelion.org>
2021-03-02 22:58:03 +0900

    사용자계정 테스트 커밋

--- src/main/java/com/itevent/iteventapi/main/MainController.java
+++ src/main/java/com/itevent/iteventapi/main/MainController.java
@@ -20,7 +20,7 @@ public class MainController {
         for (int i = 1; i <= 20; i++) {
             Map<String, Object> member = new HashMap<>();
             member.put("idx", i);
-            member.put("nickname", i + "길동");
+            member.put("nickname", i + "길똥");
             member.put("height", i + 20);
             member.put("weight", i + 30);
             members.put(i, member);
