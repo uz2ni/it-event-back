3d02b84ca7be85af09a74a2466605e16243bcac9
길승세 <rlftmdtp@hanmail.net>
2021-03-02 22:45:22 +0900

    [설정] main Controller, SecurityConfig 생성

--- src/main/java/com/itevent/iteventapi/main/MainController.java
+++ src/main/java/com/itevent/iteventapi/main/MainController.java
@@ -2,17 +2,29 @@ package com.itevent.iteventapi.main;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class MainController {
 
 	@GetMapping("/")
-	public String home(@CurrentAccount Account account, Model model) {
-		if (account != null) {
-			model.addAttribute(account);
-		}
+    @ResponseBody
+	public Map<Integer, Object> home() {
 
-		return "index";
+        Map<Integer, Object> members = new HashMap<>();
+
+        for (int i = 1; i <= 20; i++) {
+            Map<String, Object> member = new HashMap<>();
+            member.put("idx", i);
+            member.put("nickname", i + "길동");
+            member.put("height", i + 20);
+            member.put("weight", i + 30);
+            members.put(i, member);
+        }
+        return members;
 	}
 }
